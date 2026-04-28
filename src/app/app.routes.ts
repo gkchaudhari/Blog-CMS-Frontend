52e8c36b5cf2336094cdeee874710cdb2482b946
ganeshbyte <gc930954@gmail.com>
2026-04-28 09:20:01 +0530

    update the folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,23 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    loadComponent: () => import('../modules/auth/components/login/login').then((m) => m.Login),
-  },
-  {
-    path: 'signup',
-    loadComponent: () => import('../modules/auth/components/signup/signup').then((m) => m.Signup),
-  },
-  {
     path: '',
-    loadComponent: () => import('../layout/layout.component').then((m) => m.LayoutComponent),
+    redirectTo: 'layout/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'layout',
+    loadComponent: () => import('./layout/layout.component').then((m) => m.LayoutComponent),
     children: [
-      // default route after login
       {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
+        path: 'login',
+        loadComponent: () => import('./modules/auth/components/login/login').then((m) => m.Login),
       },
-    ],
+      {
+        path: 'signup',
+        loadComponent: () => import('./modules/auth/components/signup/signup').then((m) => m.Signup),
+      },
+    ]
   },
 
   // ❌ Wildcard
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -4,21 +4,21 @@ import { CommonModule } from '@angular/common';
 import { Sidebar } from './sidebar/sidebar';
 import { Navbar } from './navbar/navbar';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatNavList } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [RouterOutlet, Navbar, Sidebar, CommonModule, RouterOutlet,
-    MatSidenavModule],
+    MatSidenavModule, MatToolbar, MatIcon, MatNavList, MatButtonModule],
   templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-sidenav = viewChild<MatSidenav>('sidenav');
-
-  isOpen = signal(true);
-
-  toggleSidebar() {
-    this.isOpen.update(v => !v);
-    this.sidenav()?.toggle();
-  }
+  sidenav = viewChild<MatSidenav>('sidenav');
+  isLoggedIn = signal<boolean>(false);
+  isOpen = signal(false);
 }
